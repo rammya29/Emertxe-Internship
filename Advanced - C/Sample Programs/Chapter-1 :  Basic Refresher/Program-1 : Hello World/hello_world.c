b418447a95f18dfc5f719a65341dfc73badb7b9a
Rammya Dharshini K <70591317+rammya29@users.noreply.github.com>
2021-07-17 13:16:24 +0530

    Create hello_world.c

---
+++ Advanced - C/Sample Programs/Chapter-1 :  Basic Refresher/Program-1 : Hello World/hello_world.c
@@ -0,0 +1,21 @@
+/*-----------------------------------------------------------------------------
+ * Author      - Rammya Dharshini K
+ * Date        - Sat 17 Jul 2021 13:10:15
+ * File        - C_Sample_Chapter-1_Program-1_hello_world.c
+ * Title       - First C code
+ * Description - Every programmer would like to get the first code running on 
+                 a system wwhich is written using specific programming language.
+                 The below exmple print " hello world" on screen using C 
+                 Programming language.
+ *----------------------------------------------------------------------------*/
+ 
+ 
+#include<stdio.h>
+
+int main(){
+  
+  printf("Hello World");
+  
+  return 0;
+  
+}
