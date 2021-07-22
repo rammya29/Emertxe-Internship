5fd639157ebbf435c87bd41d5e3ff50eebbbc99f
Rammya Dharshini K <70591317+rammya29@users.noreply.github.com>
2021-07-22 09:52:12 +0530

    Create break1.c

---
+++ Advanced - C/Sample Programs/Chapter-1 :  Basic Refresher/Program-26 : Flow Control - Break - P2/break1.c
@@ -0,0 +1,42 @@
+/*----------------------------------------------------------------------------------------------- 
+ *   Author         : Rammya Dharshini K
+ *   Date           : Thur 22 Jul 2021 09:40:04 IST
+ *   File           : C_Sample_Chapter-1_Program-26_break.c
+ *   Title          : Flow control statement - break statement
+ *   Description    : As we all know that a program generally execute in an sequence unless and 
+ *                    until we have some condition to change the flow of execution. C Programming 
+ *                    Language supports different flow conditions. This program demonstrates 
+ *                    break statement
+ *----------------------------------------------------------------------------------------------*/
+ 
+#include <stdio.h>
+
+int main()
+{
+    int i, number;
+
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    for (i = 0; i < 10; i++)
+    {
+        if (i == number)
+        {
+            printf("Number matchedn");
+
+            /* 
+             * Breaks the multi itretive loops (for, while and do while) 
+             * under which it is stated 
+             * One exception is the swtich statement
+             */
+            break;
+        }
+    }
+
+    if (i == 10)
+    {
+        printf("Number not matchedn");
+    }
+
+    return 0;
+}
